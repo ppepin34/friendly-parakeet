66315a30119e3f20b0db6033d26322874559e5bb
Philip Pepin <ppepin34@gmail.com>
2022-03-11 20:49:44 -0500

    inital prompt codes

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,37 @@
 // Assignment code here
 
+// Define values
+
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialChars = "@#$%^&*";
+
+// Assign length
+
+var pwdLength = prompt("Please select a password length. Your password must be at least 8 and no more than 128 characters long.");
+
+// Prompt for lowercase
+
+var lwrcaseConfirm = confirm("Would you like to include lowercase letters?");
+
+// Prompt for uppercase
+
+var uprcaseConfirm = confirm("Would you like to use uppercase letters?");
+
+// Prompt for numbers
+
+var numbersConfirm = confirm("Would you like to include numbers?");
+
+// Prompt for special characters
+
+var specialCharsConfirm = confirm("Would you like to include special characters?");
+
+// Begin building code
+
+var generatePassword = function() {
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
