3d79b8a3ba7475e1d3a84a38ca099a0d30f77180
Philip Pepin <ppepin34@gmail.com>
2022-03-20 14:26:59 -0400

    prompts run on button click, prevent blank, improper password lengths, no selection of characters

--- Develop/script.js
+++ Develop/script.js
@@ -7,31 +7,75 @@ const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const specialChars = "@#$%^&*";
 
-// Assign length
-
-var pwdLength = prompt("Please select a password length. Your password must be at least 8 and no more than 128 characters long.");
-
-// Prompt for lowercase
-
-var lwrcaseConfirm = confirm("Would you like to include lowercase letters?");
-
-// Prompt for uppercase
-
-var uprcaseConfirm = confirm("Would you like to use uppercase letters?");
-
-// Prompt for numbers
-
-var numbersConfirm = confirm("Would you like to include numbers?");
-
-// Prompt for special characters
-
-var specialCharsConfirm = confirm("Would you like to include special characters?");
-
 // Begin building code
 
-var generatePassword = function() {
+var generatePassword = function () {
+  // Assign length
+
+  var pwdLength = parseInt(prompt("Please select a password length. Your password must be at least 8 and no more than 128 characters long."));
   
+  //debugger
+  if (pwdLength === 0 || pwdLength === NaN) {
+    alert("Please choose a password length");
+    return "";
+  };
+  
+  if (pwdLength < 8 || pwdLength > 128) {
+    alert("Please choose an appropriate password lenghth");
+    return "";
+  };
+
+  // Prompt for lowercase
+
+  var lwrcaseConfirm = confirm("Would you like to include lowercase letters?");
+
+  // Prompt for uppercase
+
+  var uprcaseConfirm = confirm("Would you like to use uppercase letters?");
+
+  // Prompt for numbers
+
+  var numbersConfirm = confirm("Would you like to include numbers?");
+
+  // Prompt for special characters
+
+  var specialCharsConfirm = confirm("Would you like to include special characters?");
+
+  // creat value for characters in password
+  var characters = "";
+
+  //check prompts for which characters to include
+  if (lwrcaseConfirm) {
+    characters += lowercase;
+  };
+
+  if (uprcaseConfirm) {
+    characters += uppercase
+  };
+
+  if (numbersConfirm){
+    characters += numbers
+  };
+
+  if (specialCharsConfirm) {
+    characters += specialChars
+  };
+
+  if (characters === "") {
+    alert("You need to select characters for the password");
+    return "";
+  };
+
+  //if none included send back to beginning
+
+  var password = "";
+  for (var i = 0; i < pwdLength; i++) {
+    
+
+
+  }
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
